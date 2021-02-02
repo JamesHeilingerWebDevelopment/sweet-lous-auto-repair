6cad84879a4c9b45b7a9447c14e6564cc87e2e4a
TheOriginalSpaceMonkey <SpaceMonkeyLab@users.noreply.github.com>
2021-02-01 16:17:47 -0800

    Added a thank-you page and a 404 page. Fixed the side navbar slide out.

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,11 @@
 function openNav() {
   document.getElementById('side-nav').style.width = '200px';
-  // document.getElementById('content').style.marginLeft = '200px';
+  // document.getElementById('content').style.marginRight = '200px';
   // document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
 }
 
 function closeNav() {
   document.getElementById('side-nav').style.width = '0';
-  // document.getElementById('content').style.marginLeft = '0';
+  // document.getElementById('content').style.marginRight = '0';
   // document.body.style.backgroundColor = 'white';
 }
