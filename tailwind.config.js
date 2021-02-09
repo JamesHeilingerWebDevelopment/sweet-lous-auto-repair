c85942cd92118c9b990bc69e73cdae102efb4e01
TheOriginalSpaceMonkey <SpaceMonkeyLab@users.noreply.github.com>
2021-02-08 16:43:06 -0800

    Fixed the navbar for larger screens, fixed the font in the sidenav, moved all css to the tailwind.css file, deleted custom.css file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [],
+  purge: [
+    './dist/**/*.html',
+    './dist/**/*.js'
+  ],
   darkMode: false,
   theme: {
     extend: {
       boxShadow: {
-        'sharp': 'box-shadow: 5px 5px 0px 0px rgba(35,31,32,1)'
+        'sharp': '5px 5px 0px 0px rgba(35,31,32,1)'
       },
       colors: {
         'mc-blue': {
@@ -20,38 +23,9 @@ module.exports = {
           '700': '#227073',
           '800': '#174a4c',
           '900': '#0b2526',
-          lighter: '#CEE9E8',
-          light: '#B4E0E1',
-          DEFAULT: '#5CCACE',
-          dark: '#3ABEC0',
-          darker: '#02ACA9',
-        },
-        'mc-pink': {
-          light: '#F596B6',
-          DEFAULT: '#F386AF',
-          dark: '#EF689B',
-          '50': '#fde7ef',
-          '100': '#facfdf',
-          '200': '#f8b6cf',
-          '300': '#f59ebf',
-          '400': '#F386AF',
-          '500': '#ed4d89',
-          '600': '#e51764',
-          '700': '#ab114b',
-          '800': '#720b32',
-          '900': '#390619',
         },
         'mc-black': {
           DEFAULT: '#231F20'
-        },
-        'mc-white': {
-          DEFAULT: '#FCFAFB'
-        },
-        'mc-yellow': {
-          DEFAULT: '#EAA80B'
-        },
-        'mc-brown': {
-          DEFAULT: '#ce995c'
         },
         'mc-harvest': {
           '50': '#fae4d0',
